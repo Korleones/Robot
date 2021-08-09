64db4c222c27c8c8789aae91b48d7205c14ef6f3
Aoiiiiiiii <1845056786@qq.com>
2021-08-09 13:57:26 +0800

    8/9 手感调整及地形
    爬墙跳手感优化
    跳跃手感优化
    爬墙跳bug修复（在同一侧墙上只能跳跃一次的规定）
    增加地形：镜子、可移动平台、地刺、可翻转地刺（由于血量与攻击系统未引入，该项未完全完成）

---
+++ Assets/BasicMovement-Aoi/TerrainScripts/Mirror.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : Platform
+{
+    public float resetTime;
+    float Timer = 0f;
+    bool TimerOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if(TimerOpen)
+        {
+            Timer += Time.fixedDeltaTime;
+            if(Timer > resetTime)
+            {
+                TimerOpen = false;
+                Timer = 0;
+                GetComponent<SpriteRenderer>().enabled = true;
+                GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+    }
+    public void Break()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        TimerOpen = true;
+    }
+}
---
+++ Assets/BasicMovement-Aoi/TerrainScripts/ReversiblThorn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ReversiblThorn : Thorn
+{
+    public float turnDuration;
+    public float turnOffsetDuration;        //平台翻转前的缓冲时间
+    bool turnTimerOpen = false;
+    float turnTimer = 0f;
+    int turnDirection = 1;
+    float turnOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            TurnOver();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (turnTimerOpen)
+        {
+            turnTimer += Time.deltaTime;
+            if (turnTimer > turnOffsetDuration)
+            {
+                transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + 180 * turnDirection / turnDuration * Time.fixedDeltaTime);
+                if (turnDirection > 0 && (180 - transform.eulerAngles.z) < 2)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 180);
+                    turnDirection *= -1;
+                    turnTimerOpen = false;
+                    turnTimer = 0;
+                }
+                else if (turnDirection < 0 && transform.eulerAngles.z < 2)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    turnDirection *= -1;
+                    turnTimerOpen = false;
+                    turnTimer = 0;
+                }
+            }
+        }
+    }
+    public void TurnOver()
+    {
+        if(!turnTimerOpen)
+        {
+            turnTimerOpen = true;
+        }
+    }
+}
